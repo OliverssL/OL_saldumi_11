c44d9ca23d36b25fb82736a4ffb694fe4ebd7f7c
OliverssL <oliverslaizans@gmail.com>
2023-03-07 09:58:37 +0200

    Pievienots atlikums

--- OL_saldumi_11/Form1.Designer.cs
+++ OL_saldumi_11/Form1.Designer.cs
@@ -48,6 +48,12 @@
             this.textBox6 = new System.Windows.Forms.TextBox();
             this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
             this.Vārds = new System.Windows.Forms.Label();
+            this.nauda1 = new System.Windows.Forms.TextBox();
+            this.nauda3 = new System.Windows.Forms.TextBox();
+            this.nauda2 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.atlikums = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -56,7 +62,7 @@
             this.Eiro.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
             this.Eiro.Location = new System.Drawing.Point(255, 94);
             this.Eiro.Name = "Eiro";
-            this.Eiro.Size = new System.Drawing.Size(192, 24);
+            this.Eiro.Size = new System.Drawing.Size(169, 24);
             this.Eiro.TabIndex = 1;
             this.Eiro.TextChanged += new System.EventHandler(this.Eiro_TextChanged);
             // 
@@ -187,7 +193,7 @@
             // 
             this.textBox2.Location = new System.Drawing.Point(255, 163);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(192, 20);
+            this.textBox2.Size = new System.Drawing.Size(169, 20);
             this.textBox2.TabIndex = 15;
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged_1);
             // 
@@ -195,7 +201,7 @@
             // 
             this.textBox3.Location = new System.Drawing.Point(255, 205);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(192, 20);
+            this.textBox3.Size = new System.Drawing.Size(169, 20);
             this.textBox3.TabIndex = 16;
             this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
@@ -203,7 +209,7 @@
             // 
             this.textBox4.Location = new System.Drawing.Point(255, 247);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(192, 20);
+            this.textBox4.Size = new System.Drawing.Size(169, 20);
             this.textBox4.TabIndex = 17;
             this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
             // 
@@ -211,7 +217,7 @@
             // 
             this.textBox5.Location = new System.Drawing.Point(255, 124);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(192, 20);
+            this.textBox5.Size = new System.Drawing.Size(169, 20);
             this.textBox5.TabIndex = 18;
             this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
             // 
@@ -249,12 +255,70 @@
             this.Vārds.Text = "Vārds";
             this.Vārds.Click += new System.EventHandler(this.label2_Click_1);
             // 
+            // nauda1
+            // 
+            this.nauda1.Location = new System.Drawing.Point(441, 124);
+            this.nauda1.Name = "nauda1";
+            this.nauda1.Size = new System.Drawing.Size(143, 20);
+            this.nauda1.TabIndex = 24;
+            this.nauda1.TextChanged += new System.EventHandler(this.nauda1_TextChanged);
+            // 
+            // nauda3
+            // 
+            this.nauda3.Location = new System.Drawing.Point(441, 247);
+            this.nauda3.Name = "nauda3";
+            this.nauda3.Size = new System.Drawing.Size(143, 20);
+            this.nauda3.TabIndex = 25;
+            this.nauda3.TextChanged += new System.EventHandler(this.nauda3_TextChanged);
+            // 
+            // nauda2
+            // 
+            this.nauda2.Location = new System.Drawing.Point(441, 205);
+            this.nauda2.Name = "nauda2";
+            this.nauda2.Size = new System.Drawing.Size(143, 20);
+            this.nauda2.TabIndex = 26;
+            this.nauda2.TextChanged += new System.EventHandler(this.nauda2_TextChanged);
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(441, 167);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(143, 20);
+            this.textBox10.TabIndex = 27;
+            this.textBox10.TextChanged += new System.EventHandler(this.textBox10_TextChanged);
+            // 
+            // atlikums
+            // 
+            this.atlikums.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.atlikums.Location = new System.Drawing.Point(312, 471);
+            this.atlikums.Name = "atlikums";
+            this.atlikums.Size = new System.Drawing.Size(249, 35);
+            this.atlikums.TabIndex = 28;
+            this.atlikums.TextChanged += new System.EventHandler(this.atlikums_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label2.Location = new System.Drawing.Point(131, 471);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 25);
+            this.label2.TabIndex = 29;
+            this.label2.Text = "Atlikums";
+            this.label2.Click += new System.EventHandler(this.label2_Click_2);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.WhiteSmoke;
             this.ClientSize = new System.Drawing.Size(1217, 574);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.atlikums);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.nauda2);
+            this.Controls.Add(this.nauda3);
+            this.Controls.Add(this.nauda1);
             this.Controls.Add(this.Vārds);
             this.Controls.Add(this.monthCalendar1);
             this.Controls.Add(this.textBox6);
@@ -309,6 +373,12 @@
         private System.Windows.Forms.TextBox textBox6;
         private System.Windows.Forms.MonthCalendar monthCalendar1;
         private System.Windows.Forms.Label Vārds;
+        private System.Windows.Forms.TextBox nauda1;
+        private System.Windows.Forms.TextBox nauda3;
+        private System.Windows.Forms.TextBox nauda2;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox atlikums;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- OL_saldumi_11/Form1.cs
+++ OL_saldumi_11/Form1.cs
@@ -56,16 +56,26 @@ namespace OL_saldumi_11
                 sk1 = Convert.ToDouble(Eiro.Text);
                 cena = (cena1 + cena2 + cena3 + cena4 + cena5) / skaits;
                 textBox1.Text = (sk1 / cena).ToString();//izvada rezultatu textBox1 
+
+
             }
-    
-        catch(Exception ex)
-        {
+
+            catch (Exception ex)
+            {
                 MessageBox.Show("Nebija ievadīti korekti dati");
                 MessageBox.Show(ex.Message);
-        }
+            }
+            nauda1.Text = (kg4 * cena5).ToString();
+            textBox10.Text = (kg2 * cena3).ToString();
+            nauda2.Text = (kg3 * cena4).ToString();
+            nauda3.Text = (kg1 * cena2).ToString();
+            atlikums.Text = (sk1 - cena).ToString();
         }
 
-private void textBox1_TextChanged(object sender, EventArgs e)
+
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
@@ -83,7 +93,7 @@ private void textBox1_TextChanged(object sender, EventArgs e)
         }
 
         private void Migle_CheckedChanged(object sender, EventArgs e)
-        {try
+        { try
             {
                 if (Migle.Checked == true)
                 {
@@ -92,147 +102,133 @@ private void textBox1_TextChanged(object sender, EventArgs e)
                     kg1 = Convert.ToDouble(textBox4.Text);
                     cena8 = kg1 * cena2;
                 }
-                else
-                {
-                    cena1 = 9.88;
-                }
             }
-            catch (Exception ex)
+            catch
             {
                 MessageBox.Show("Nebija ievadīti korekti dati");
-                MessageBox.Show(ex.Message);
             }
-
-            }
+        }
 
         private void Rudzupuķe_CheckedChanged(object sender, EventArgs e)
         {
             try
             {
                 if (Rudzupuķe.Checked == true)
-                {
                     cena3 = 12.19;
-                    skaits = skaits + 1;
-                    kg2 = Convert.ToDouble(textBox2.Text);
-                    cena7 = kg2 * cena3;
-                }
-                else
-                {
-                    cena1 = 9.88;
-                }
+                skaits = skaits + 1;
+                kg2 = Convert.ToDouble(textBox2.Text);
+                cena7 = kg2 * cena3;
+
             }
-            catch (Exception ex)
+            catch
             {
                 MessageBox.Show("Nebija ievadīti korekti dati");
-                MessageBox.Show(ex.Message);
             }
-            }
+        }
 
         private void Serenāde_CheckedChanged(object sender, EventArgs e)
-        {try
+        { try
             {
                 if (Serenāde.Checked == true)
-                {
-                    cena3 = 16.99;
-                    skaits = skaits + 1;
-                    kg3 = Convert.ToDouble(textBox3.Text);
-                    cena5 = kg3 * cena3;
-                }
-                else
-                {
-                    cena1 = 9.88;
-                }
+
+                    cena4 = 16.99;
+                skaits = skaits + 1;
+                kg3 = Convert.ToDouble(textBox3.Text);
+                cena5 = kg3 * cena4;
+
             }
-            catch (Exception ex)
+            catch
             {
                 MessageBox.Show("Nebija ievadīti korekti dati");
-                MessageBox.Show(ex.Message);
             }
-            }
+        }
 
         private void Gotiņa_CheckedChanged(object sender, EventArgs e)
         {
             try
             {
                 if (Gotiņa.Checked == true)
-                {
-                    cena4 = 10.69;
-                    skaits = skaits + 1;
-                    kg4 = Convert.ToDouble(textBox5.Text);
-                    cena6 = kg4 * cena5;
-                }
-                else
-                {
-                    cena1 = 9.88;
-                }
+                    cena5 = 10.69;
+                skaits = skaits + 1;
+                kg4 = Convert.ToDouble(textBox5.Text);
+                cena6 = kg4 * cena5;
             }
-            catch (Exception ex)
+            catch
             {
                 MessageBox.Show("Nebija ievadīti korekti dati");
-                MessageBox.Show(ex.Message);
             }
-
-            }
-
+        }
         private void label4_Click(object sender, EventArgs e)
         {
-
         }
-
         private void fontDialog1_Apply(object sender, EventArgs e)
         {
-
         }
-
         private void textBox4_TextChanged(object sender, EventArgs e)
         {
-           
         }
-
         private void textBox3_TextChanged(object sender, EventArgs e)
         {
-            
         }
-
         private void textBox2_TextChanged_1(object sender, EventArgs e)
         {
-            
         }
-
         private void textBox5_TextChanged(object sender, EventArgs e)
         {
-        
         }
-
         private void label2_Click_1(object sender, EventArgs e)
         {
-
         }
-
         private void button2_Click(object sender, EventArgs e)
         {
-            { 
-            string vards = this.textBox6.Text;
-            string kg = this.textBox1.Text;
-            string failanosaukums = vards + "_" + DateTime.Now.ToString("ddMMyyyy") + "_ceks.txt";
-            StreamWriter musu_faila_ierakstitajs = new StreamWriter(failanosaukums);
-            musu_faila_ierakstitajs.WriteLine(vards);
-            musu_faila_ierakstitajs.WriteLine(kg);
-            musu_faila_ierakstitajs.Close();
-            }
-            if (File.Exists(filepath))
-                {
+            {
+                string vards = this.textBox6.Text;
+                string kg = this.textBox1.Text;
+                string failanosaukums = vards + "_" + DateTime.Now.ToString("ddMMyyyy") + "_ceks.txt";
+                StreamWriter musu_faila_ierakstitajs = new StreamWriter(failanosaukums);
+                musu_faila_ierakstitajs.WriteLine(vards);
+                musu_faila_ierakstitajs.WriteLine(kg);
+                musu_faila_ierakstitajs.Close();
             }
         }
 
         private void textBox6_TextChanged(object sender, EventArgs e)
         {
+        }
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void label2_Click_2(object sender, EventArgs e)
+        {
 
         }
 
-        private void textBox7_TextChanged(object sender, EventArgs e)
+        private void atlikums_TextChanged(object sender, EventArgs e)
         {
+            
+        }
 
+
+private void nauda1_TextChanged(object sender, EventArgs e)
+        {
+            
+            string cena = this.nauda1.Text;
+        }
+
+        private void textBox10_TextChanged(object sender, EventArgs e)
+        {
+            string cena = this.textBox10.Text;
+        }
+
+        private void nauda2_TextChanged(object sender, EventArgs e)
+        {
+            string cena = this.nauda2.Text;
+        }
+
+        private void nauda3_TextChanged(object sender, EventArgs e)
+        {
+            string cena = this.nauda3.Text;
         }
     }
 }
